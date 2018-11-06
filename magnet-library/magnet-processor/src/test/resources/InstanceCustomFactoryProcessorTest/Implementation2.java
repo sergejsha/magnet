9e7889f348e6faf8822ccc979b0861177411b4fc
Sergej Shafarenka <beworker@gmail.com>
2018-11-06 18:36:35 +0100

    Fixing unit test

--- magnet-library/magnet-processor/src/test/resources/InstanceCustomFactoryProcessorTest/Implementation2.java
+++ magnet-library/magnet-processor/src/test/resources/InstanceCustomFactoryProcessorTest/Implementation2.java
@@ -3,7 +3,7 @@ package test;
 import magnet.Instance;
 
 @Instance(
-    type = Interface1.class,
+    type = Interface2.class,
     factory = CustomFactory2.class
 )
 public class Implementation2 implements Interface2 {}
