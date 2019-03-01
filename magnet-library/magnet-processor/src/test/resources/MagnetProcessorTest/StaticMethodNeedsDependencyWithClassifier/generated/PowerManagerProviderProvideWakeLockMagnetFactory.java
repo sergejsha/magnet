57e924be26109e01b915f9066ed22a918b05e504
Thomas Keller <me@thomaskeller.biz>
2019-03-02 00:20:38 +0100

    Update PowerManagerProviderProvideWakeLockMagnetFactory.java

--- magnet-library/magnet-processor/src/test/resources/MagnetProcessorTest/StaticMethodNeedsDependencyWithClassifier/generated/PowerManagerProviderProvideWakeLockMagnetFactory.java
+++ magnet-library/magnet-processor/src/test/resources/MagnetProcessorTest/StaticMethodNeedsDependencyWithClassifier/generated/PowerManagerProviderProvideWakeLockMagnetFactory.java
@@ -9,7 +9,7 @@ public final class PowerManagerProviderProvideWakeLockMagnetFactory extends Inst
 
     @Override
     public PowerManager.WakeLock create(Scope scope) {
-        Context context = scope.getSingle(Context.class, "application ");
+        Context context = scope.getSingle(Context.class, "application");
         return PowerManagerProvider.provideWakeLock(context);
     }
 
